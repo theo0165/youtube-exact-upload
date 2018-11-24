aeaae61bc6a595bfcb7a445c1ae80062d48331d4
Theo Sandell <theo01sandell@gmail.com>
2018-11-25 00:36:47 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# youtube-exact-upload
+# Youtube exact upload
 Tampermonkey script that adds hours and minutes to upload date on youtube
+
+#### INSTALLATION
+1. Install the tampermonkey script. [Click here!](youtube-exact-upload.user.js)
+2. Generate Youtube Data V3 API keys [Click here!](https://console.developers.google.com/apis/api/youtube.googleapis.com/overview)
+3. Set the API_KEY variable in the script
+4. Done!
---
+++ youtube-exact-upload.user.js
@@ -0,0 +1,56 @@
+// ==UserScript==
+// @name         Youtube exact upload
+// @namespace    youtube-exact-upload
+// @version      0.1
+// @description  Adds exact upload time to youtube videos
+// @author       Theo Sandell
+// @match        https://www.youtube.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var YT_API_KEY = "";
+
+    console.log("YT EXACT UPLOAD LOADED");
+
+    var BASE_URL = "https://www.googleapis.com/youtube/v3/videos?part=snippet&key=" + YT_API_KEY;
+
+    function genUrl(){
+        const urlParams = new URLSearchParams(window.location.search);
+
+        if(urlParams.get("v") != null){
+            return BASE_URL + "&id=" + urlParams.get("v");
+        }else {
+            return "";
+        }
+    }
+
+    function parseDate(date){
+        var months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
+
+        return date.getDate() + " " + months[date.getMonth()] + ". " + date.getFullYear() + " " + (date.getHours() < 10 ? date.getHours() + "0" : date.getHours()) + ":" + (date.getMinutes() < 10 ? date.getMinutes() + "0" : date.getMinutes());
+    }
+
+    if(YT_API_KEY != "" || typeof YT_API_KEY != "undefined"){
+        var url = genUrl();
+
+        if(url != ""){
+            fetch(url)
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(data){
+                if(data.pageInfo.totalResults > 0){
+                    var element = document.querySelector(".date");
+                    var date = new Date(data.items[0].snippet.publishedAt);
+
+                    element.innerHTML = "Published at " + parseDate(date);
+                }
+            })
+            .catch(error => console.error("YOUTUBE EXACT UPLOAD ERROR: " + error, "\nINVALID API KEY?"))
+        }
+    }else{
+        console.error("YOUTUBE EXACT UPLOAD ERROR: Undefined api key");
+    }
+})();
